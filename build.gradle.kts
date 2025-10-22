a81117442405bffdea5df31b1a7e84c57a55ab6e
alejandro-hs <alejandro@helpscout.com>
2025-10-21 22:20:12 -0500

    fix: add Kotlin plugin and OpenAPI spec to resolve build errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.6"
-    id("io.spring.dependency-management") version "1.1.7"
+    id("hs-openapi-conventions")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.jetbrains.kotlin.jvm")
     id("org.graalvm.buildtools.native") version "0.10.6"
 }
 
@@ -11,14 +11,15 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 description = "springnative"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/helpscout/*")
+        credentials {
+            username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("HS_GITHUB_PACKAGES_USER")
+            password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("HS_GITHUB_PACKAGES_TOKEN")
+        }
+    }
 }
 
 dependencies {
@@ -29,9 +30,9 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-kotlin {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+        freeCompilerArgs.add("-Xjsr305=strict")
     }
 }
 
